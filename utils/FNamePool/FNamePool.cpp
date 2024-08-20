a6f31417a205b531ced40cd0a2ab498b5b9384a3
yring <yangyt67@mail2.sysu.edu.cn>
2024-08-20 17:38:34 +0800

    add game switcher

--- utils/FNamePool/FNamePool.cpp
+++ utils/FNamePool/FNamePool.cpp
@@ -1,9 +1,12 @@
 #include "FNamePool.h"
-
+#include "../GAME.h"
 extern "C" int32_t get_FNamePool_Stride()
 {
-    // return FNameEntryAllocator::Stride;
+#ifdef COM_NETEASE_RACE
     return 4;
+#endif
+    // return FNameEntryAllocator::Stride;
+    return 2;
 }
 
 extern "C" int32_t get_FNamePool_CurrentBlock()
