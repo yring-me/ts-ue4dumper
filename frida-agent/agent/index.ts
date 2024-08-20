a162d1c864438d973b7694ea4c7288d40a89835a
yring <yangyt67@mail2.sysu.edu.cn>
2024-08-20 19:11:08 +0800

    delete Global Offset

--- frida-agent/agent/index.ts
+++ frida-agent/agent/index.ts
@@ -1,18 +1,9 @@
 var GWorld: NativePointer = ptr(0);
-export var GNames: NativePointer = ptr(0);
+var GNames: NativePointer = ptr(0);
 var GUObjectArray: NativePointer = ptr(0);
 var ue4: NativePointer | null = ptr(0);
 import { dumpActor, dumpSDK, dumpActorName } from "./dump.js";
-
-// com.netease.race
-// var GNames_offset = 0x15D458C0
-// var GWorld_offset = 0x15F1CE60
-// var GUObjectArray_offset = 0x15DA0788
-
-// com.ShuiSha.FPS2
-var GNames_offset = 0xAE7B500
-var GWorld_offset = 0xB036900
-var GUObjectArray_offset = 0xAEBF7D8
+import { GNames_offset, GWorld_offset, GUObjectArray_offset } from "./offset.js"
 
 function init() {
     ue4 = Module.findBaseAddress("libUE4.so");
